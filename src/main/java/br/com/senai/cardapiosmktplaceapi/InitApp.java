da457420d2107fbcbfb7b840f9668c6911fc1d94
Gustavo D. Tonetto <gustavotonetto1@gmail.com>
2023-09-14 20:03:52 -0300

    arrumando gitignore

--- src/main/java/br/com/senai/cardapiosmktplaceapi/InitApp.java
+++ src/main/java/br/com/senai/cardapiosmktplaceapi/InitApp.java
@@ -1,32 +1,13 @@
 package br.com.senai.cardapiosmktplaceapi;
 
-import java.util.List;
-
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
-import org.springframework.data.domain.PageRequest;
-import org.springframework.data.domain.Pageable;
-
-import br.com.senai.cardapiosmktplaceapi.entity.Categoria;
-import br.com.senai.cardapiosmktplaceapi.entity.Endereco;
-import br.com.senai.cardapiosmktplaceapi.entity.Restaurante;
-import br.com.senai.cardapiosmktplaceapi.entity.enums.Status;
-import br.com.senai.cardapiosmktplaceapi.entity.enums.TipoDeCategoria;
-import br.com.senai.cardapiosmktplaceapi.repository.CategoriasRepository;
-import br.com.senai.cardapiosmktplaceapi.repository.RestaurantesRepository;
 
 @SpringBootApplication
 public class InitApp {
-	
-	@Autowired
-	private CategoriasRepository categoriasRepository;
-	
-	@Autowired
-	private RestaurantesRepository restaurantesRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(InitApp.class, args);
